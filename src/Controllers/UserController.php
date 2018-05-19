5827b8152ff91ad8b197b7f55b76dc10fb528f5a
Sergeilomako <flameseeker2304@gmail.com>
2018-05-19 18:12:27 +0300

    register() ver. 1.0

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Mindk\Framework\Exceptions\AuthRequiredException;
 use Mindk\Framework\Http\Request\Request;
 use Mindk\Framework\Models\UserModel;
 use Mindk\Framework\DB\DBOConnectorInterface;
+use Mindk\Framework\Http\Response\JsonResponse;
 
 /**
  * Class UserController
@@ -14,7 +15,39 @@ use Mindk\Framework\DB\DBOConnectorInterface;
 class UserController
 {
     public function register(Request $request, UserModel $model) {
-        //@TODO: Implement
+        print_r('test11');
+        $errors = [];
+        $email = null;
+        $password = null;
+        if($request->get('email', '', 'email')){
+            if(!empty($model->findByEmail($request->get('email', '', 'email')))){
+                $email = $request->get('email', '', 'email');
+            }
+            else {
+                array_push($errors, ['email' => 'Email already exists']);
+            }
+        }
+        if(strlen($request->get('password', '')) > 5 && $request->get('password', '') === $request->get('confirm_password', '')){
+            $password = $request->get('password', '');
+        }
+        elseif(strlen($request->get('password', '')) <= 5){
+            array_push($errors, ['password' => 'password must be at least 6 characters']);
+        }
+        else {
+            array_push($errors, ['password' => 'passwords do not match']);
+        }
+
+        $status = null;
+        if(empty($errors)){
+            $model->create(['email' => $email, 'password' => password_hash($password, PASSWORD_DEFAULT)]);
+            $status = 'success';
+        }
+        else {
+            $status = $errors;
+        }
+        $response = new JsonResponse($status);
+        $response->send();
+
     }
 
     /**
