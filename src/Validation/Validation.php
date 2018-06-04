173b5118d0c57936c348a2b2ce84c71c3f89b128
Sergeilomako <flameseeker2304@gmail.com>
2018-06-05 01:00:00 +0300

    fix Validator: add string() and add int()

--- src/Validation/Validation.php
+++ src/Validation/Validation.php
@@ -15,8 +15,18 @@ use Mindk\Framework\DB\DBOConnectorInterface;
 class Validation
 {
     protected $db;
-    protected $rules = ['min', 'max', 'file', 'email', 'required', 'confirmed', 'unique'];
+    protected $rules = ['min', 'max', 'file', 'email', 'required',
+                        'confirmed', 'unique', 'string', 'int'];
 
+    /**
+     * Validate request
+     *
+     * @param Request $request
+     * @param $data
+     * @param DBOConnectorInterface $db
+     * @return array|bool
+     * @throws ValidationException
+     */
     public function validate(Request $request, $data, DBOConnectorInterface $db){
         $this->db = $db;
         $errors = [];
@@ -58,31 +68,77 @@ class Validation
         
         return empty($errors) ? true : $errors;
     }
-    
+
+    /**
+     * Check min length
+     *
+     * @param $field
+     * @param $field_value
+     * @param int $min
+     * @return array|bool
+     */
     public function min($field, $field_value, int $min) {
 
         return strlen($field_value) >= $min ? true : [$field => ucfirst($field) . " must be at least $min characters"];
     }
-    
+
+    /**
+     * Check max length
+     *
+     * @param $field
+     * @param $field_value
+     * @param int $max
+     * @return array|bool
+     */
     public function max($field, $field_value, int $max) {
 
         return strlen($field_value) <= $max ? true : [$field =>  ucfirst($field) . " must not exceed $max characters"];
     }
 
+    /**
+     * Сhecks if the field is a loaded file
+     *
+     * @param $file_field
+     * @param $field_value
+     * @return array|bool
+     */
     public function file($file_field, $field_value) {
 
         return isset($field_value['tmp_name']) && is_file($field_value['tmp_name']) ? true : [$file_field => ucfirst($file_field) . " is not a file"];
     }
-    
+
+    /**
+     * Сhecks if the field is a email
+     *
+     * @param $field
+     * @param $field_value
+     * @return array|bool
+     */
     public function email($field, $field_value) {
 
         return is_string(filter_var($field_value, FILTER_VALIDATE_EMAIL)) ? true : [$field => "Incorrect email"];
     }
-    
+
+    /**
+     * Required field
+     *
+     * @param $field
+     * @param $field_value
+     * @return array|bool
+     */
     public function required($field, $field_value) {
         return !empty($field_value) ? true : [$field => ucfirst($field) . " is required"];
     }
-    
+
+    /**
+     * Verifies the field with the confirmation field
+     *
+     * @param $field
+     * @param $field_value
+     * @param Request $request
+     * @return array|bool
+     * @throws ValidationException
+     */
     public function confirmed($field, $field_value, Request $request){
         $confirmed_field = 'confirm_' . lcfirst($field);
         if(!$request->has($confirmed_field)){
@@ -92,6 +148,16 @@ class Validation
         return $field_value === $request->get($confirmed_field, null, 'string') ? true : [$field => ucfirst($field) . 's do not match'];
     }
 
+    /**
+     * Сhecks the field in the specified table for uniqueness
+     *
+     * @param $field
+     * @param $field_value
+     * @param $table_name
+     * @param $column
+     * @return array|bool
+     * @throws ValidationException
+     */
     public function unique($field, $field_value, $table_name, $column){
         $namespace = $table_name == 'users' ? '\Mindk\Framework\Models\\' : '\App\Models\\'; 
         $model_name = $namespace . ucfirst(substr($table_name, 0, -1)) . 'Model';
@@ -105,7 +171,29 @@ class Validation
         }
         $check = $model->exist($column, $field_value);
 
-        return empty($check) ? true : [$field => ucfirst($field) . " already exists"];
+        return empty($check) ? true : [$field => ucfirst($field) . ' already exists'];
+    }
+
+    /**
+     * Сhecks if the field is a string
+     * 
+     * @param $field
+     * @param $field_value
+     * @return array|bool
+     */
+    public function string($field, $field_value){
+        return is_string($field_value) ? true : [$field => ucfirst($field) . ' is not a string'];
+    }
+
+    /**
+     * Сhecks if the field is a integer
+     * 
+     * @param $field
+     * @param $field_value
+     * @return array|bool
+     */
+    public function int($field, $field_value){
+        return is_int($field_value) ? true : [$field => ucfirst($field) . ' is not a integer'];
     }
 
 }
