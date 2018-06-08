cc4ef1ffd66b402d887b1c9b0cfbf12bee923869
Sergeilomako <flameseeker2304@gmail.com>
2018-06-08 23:04:48 +0300

    merge

--- src/Http/Request/Request.php
+++ src/Http/Request/Request.php
@@ -45,12 +45,8 @@ class Request
         // Grab all request data:
 
         $raw_data = $_REQUEST + $_FILES;
-        if($this->getMethod() === 'PUT') {
-            parse_str($this->getRawInput(),$post_vars);
-            if(is_array(json_decode($this->getRawInput(), true))){
+        if(is_array(json_decode($this->getRawInput(), true))) {
                 $raw_data += json_decode($this->getRawInput(), true);
-            }
-            $raw_data += $post_vars;
         }
 
         // Make headers act like object:
